d6a1e85602a6a12fdaa38a1992a9167ef62679f1
Sean Reed <sreedartwork@gmail.com>
2020-02-25 12:23:21 -0500

    added feature to create food item and worked on controller

--- app/FoodItem.php
+++ app/FoodItem.php
@@ -8,9 +8,9 @@ class FoodItem extends Model
 {
     protected $table = 'food_items';
 
-    // protected $fillable = [
-    //     'title', 'description', 'image_url', 'price', 'category_id'
-    // ];
+    protected $fillable = [
+        'title', 'description', 'image_url', 'price', 'category_id'
+    ];
 
     public function categories(){
         return $this->belongsTo('App\FoodCategory');
--- resources/views/admin/food-items/create.blade.php
+++ resources/views/admin/food-items/create.blade.php
@@ -1,21 +1,20 @@
 @extends('layouts.admin')
 
 @section('content')
-
-           <div class="container-fluid  dashboard-content">
+            <div class="container-fluid  dashboard-content">
                 <!-- ============================================================== -->
                 <!-- pageheader -->
                 <!-- ============================================================== -->
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="page-header">
-                            <h2 class="pageheader-title">New Food Items</h2>
+                            <h2 class="pageheader-title">New Food Items </h2>
                             <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                             <div class="page-breadcrumb">
                                 <nav aria-label="breadcrumb">
                                     <ol class="breadcrumb">
                                         <li class="breadcrumb-item"><a href="/admin" class="breadcrumb-link">Dashboard</a></li>
-                                        <li class="breadcrumb-item"><a href="/admin/food-items" class="breadcrumb-link">All Food Items</a></li>
+                                        <li class="breadcrumb-item"><a href="/admin/food-items" class="breadcrumb-link">All  Food Items</a></li>
                                         <li class="breadcrumb-item active" aria-current="page">New Food Item</li>
                                     </ol>
                                 </nav>
@@ -26,7 +25,7 @@
                 <!-- ============================================================== -->
                 <!-- end pageheader -->
                 <!-- ============================================================== -->
-
+             
                     <div class="row">
                         <!-- ============================================================== -->
                         <!-- basic form -->
@@ -35,29 +34,65 @@
                             <div class="card">
                                 <h5 class="card-header">Create a new Item</h5>
                                 <div class="card-body">
-                                    <form action="#" id="basicform" data-parsley-validate="" novalidate="">
+                                    <form method="POST" action="/admin/food-items">
+                                        @csrf
                                         <div class="form-group">
-                                            <label for="inputItem">Item Name</label>
-                                            <input id="inputItem" type="text" name="title" data-parsley-trigger="change" required="" placeholder="Enter item name" autocomplete="off" class="form-control">
-                                        </div>
-                                        
-                                        <div class="form-group">
-                                            <label for="inputItem">Price</label>
-                                            <input id="inputItem" type="text" name="price" data-parsley-trigger="change" required="" placeholder="Enter item price" autocomplete="off" class="form-control">
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="inputItemImageUrl">Item Image Url</label>
-                                            <input id="inputItemImageUrl" type="text" name="image_url" data-parsley-trigger="change" required="" placeholder="http://www.billys.com/img/burgers.jpeg" autocomplete="off" class="form-control">
-                                        </div>
+                                            <label for="inputtitle">Title</label>
+                                            <input id="inputtitle" type="text" class="form-control form-control-lg @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="title" autofocus placeholder="Give Item a Title">
 
+                                            @error('title')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputprice">Price</label>
+                                            <input id="inputprice" type="text" class="form-control form-control-lg @error('price') is-invalid @enderror" name="price" value="{{ old('price') }}" required autocomplete="price" autofocus placeholder="Give Item a Price">
+
+                                            @error('price')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputcategory">Category</label>
+                                            <select name="category_id" class="form-control" id="inputrole">
+                                                @foreach ($categories as $category)
+                                                    <option value="{{$category->id}}"
+                                                    >{{$category->title}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputimageurl">Image Url</label>
+                                            <input id="inputimageurl" type="text" class="form-control form-control-lg @error('image_url') is-invalid @enderror" name="image_url" value="{{ old('image_url') }}" autocomplete="title" autofocus placeholder="Add the url to the Item image">
+
+                                            @error('image_url')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputdescription">Description</label>
+                                            <textarea id="inputdescription" type="text" class="form-control form-control-lg @error('description') is-invalid @enderror" name="description" required autofocus placeholder="Write a Description">{{ old('description') }}</textarea>
+
+                                            @error('description')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
                                         <div class="row">
                                             <div class="col-sm-6 pb-2 pb-sm-4 pb-lg-0 pr-0">
-
+                                                
                                             </div>
                                             <div class="col-sm-6 pl-0">
                                                 <p class="text-right">
                                                     <button type="submit" class="btn btn-space btn-primary">Submit</button>
-
+                                                    
                                                 </p>
                                             </div>
                                         </div>
@@ -67,11 +102,9 @@
                         </div>
                         <!-- ============================================================== -->
                         <!-- end basic form -->
-
+                        
                     </div>
-
+                    
             </div>
+            
 @endsection
-
-
-
